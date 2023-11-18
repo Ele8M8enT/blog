a21e92f267e6afe6d947b1d5e78b93904c46605b
Ele8M8enT <73663306+Ele8M8enT@users.noreply.github.com>
2023-11-18 15:21:57 +0100

    Update getLatestPost.php

--- getLatestPost.php
+++ getLatestPost.php
@@ -3,14 +3,14 @@ include('post.php');
 
 $post = new Post();
 
-// Pobierz czas ostatniego odpytania z sesji
+// Czas ostatniego odpytania z sesji
 session_start();
 $lastCheckTime = $_SESSION['lastCheckTime'] ?? null;
 
-// Pobierz najnowszy post od czasu ostatniego odpytania
+// Najnowszy post od czasu ostatniego odpytania
 $latestPost = $post->getLatestPostSince($lastCheckTime);
 
-// Zapisz aktualny czas jako czas ostatniego odpytania
+// Aktualny czas jako czas ostatniego odpytania
 $_SESSION['lastCheckTime'] = time();
 
 header('Content-Type: application/json');
